c1d54e56673d3f6eda4caa9958b3d4d97aede556
vagrant <vagrant@localhost.localdomain>
2020-08-16 11:56:07 +0000

    client_signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    	client_path(current_client)
+  	end
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :phone_number, :postal_code, :address])
   end
-  
+
 end
