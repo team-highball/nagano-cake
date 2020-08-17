d962dece34b00210ed8c92207439e14540118c07
akirahashibami <hashibami2207@gmail.com>
2020-08-17 12:07:58 +0000

    [on the way] client_order

--- app/controllers/client/orders_controller.rb
+++ app/controllers/client/orders_controller.rb
@@ -1,24 +1,33 @@
 class Client::OrdersController < ApplicationController
 
     def index
-        
+
     end
 
     def show
-        
+
     end
-    
+
 
     def new
-        
+      @order = Order.new
     end
-    
+
     def create
-        
+      @order = Order.new(order_params)
+      @order.client_id = current_user.id
+      @order.save
+      redirect_to client_orders_thanks_path
     end
-    
+
     def thanks
-        
+
     end
-    
+
+    private
+
+    def order_params
+      params.require(:order).permit(:status, :postal_code, :address, :name, :status)
+    end
+
 end
