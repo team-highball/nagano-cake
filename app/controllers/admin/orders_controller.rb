f46397c932d47a289aa417feba2d7cf10cbaa49e
vagrant <vagrant@localhost.localdomain>
2020-08-20 02:18:11 +0000

    first_commit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,11 +3,20 @@ class Admin::OrdersController < ApplicationController
     before_action :authenticate_admin!
 
     def index
-
+        case params[:order_sort]
+            when "0"
+                @orders = Order.where(created_at: Time.zone.now.all_day)
+            when "1"
+                @client = Client.find(params[:id])
+                @orders = @client.orders
+            else
+                @orders = Order.all
+        end
     end
 
     def show
-
+        @order = Order.find(params[:id])
+        @client = Client.find(@order.client_id)
     end
 
     def update
