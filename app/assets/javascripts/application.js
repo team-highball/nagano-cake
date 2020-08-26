73f81fbc868e3f773c1c125e013b1e01c2119a34
vagrant <vagrant@localhost.localdomain>
2020-08-26 11:59:53 +0000

    add_kana_validate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,35 @@ $(document).on('turbolinks:load', function () {
   });
 });
 
+
+
+
+
+$(function(){
+
+  function insertStr(input){
+    return input.slice(0, 3) + '-' + input.slice(3,input.length);
+  }
+
+  $("#client_postal_code").on('keyup',function(e){
+    var input = $(this).val();
+
+    var key = event.keyCode || event.charCode;
+    if( key == 8 || key == 46 ){
+      return false;
+    }
+
+    if(input.length === 3){
+      $(this).val(insertStr(input));
+    }
+  });
+
+  $("client_postal_code").on('blur',function(e){
+    var input = $(this).val();
+
+    if(input.length >= 3 && input.substr(3,1) !== '-'){
+      $(this).val(insertStr(input));
+    }
+  });
+}
+)
